944b2fac4142a49e3c8f4a2e18257853027093a6
lautariano <lautariano777@gmail.com>
2022-04-25 21:52:58 +0500

    begin create dataloader

--- NeuralNetwork/datasets.py
+++ NeuralNetwork/datasets.py
@@ -1,20 +1,26 @@
 import pandas as pd
+import numpy as np
+import tensorflow as tf
 import matplotlib.pyplot as plt
 
-weight_height = pd.read_csv('weight-height.csv')
-weight_height.loc[weight_height.Gender == "Female", 'Gender'] = 0
-weight_height.loc[weight_height.Gender == "Male", 'Gender'] = 1
-weight_height= weight_height.drop(labels=[*range(1000, 10000)], axis=0)
+filename = ("https://raw.githubusercontent.com/WISEPLAT/SBER-LSTM-Neural-Network-for-Time-Series-Prediction/master/data/SBER_000101_220128.csv")
+df = pd.read_csv(filename, sep=",")
+df.rename(columns={'<DATE>': "Date", "<TIME>": "Time", "<OPEN>": "Open", "<HIGH>": "High", "<LOW>": "Low", "<CLOSE>": "Close", "<VOL>": "Volume"}, inplace=True)
+df["Date"] = pd.to_datetime(df["Date"], format="%Y%m%d")
+df = df.drop("Time", 1)
 
-wh = weight_height.sample(frac=1)
-print(wh.head(40))
-def weight_height_plot(dataset):
-    dataset.plot(y='Height', kind='hist',
-              color='red', title='Height (inch.) distribution')
-    plt.show()
+split = 0.85
+i_split = int(len(df) * split)
+cols = ["Close", "Volume"]
+data_train = df.get(cols).values[:i_split]
+data_test = df.get(cols).values[i_split:]
 
-def get_wh():
-    return wh
+class DataLoader():
 
-if __name__ == "__main__":
-    weight_height_plot(wh)
+    def __init__(self, filename, split, cols):
+        self.df = pd.read_csv(filename)
+        i_split = int(len(df) * split)
+        self.data_train = df.get(cols).values[:i_split]
+        self.data_test = df.get(cols).values[i_split:]
+        self.len_train = len(self.data_train)
+        self.len_test = len(self.data_test)
--- NeuralNetwork/predictions-train.py
+++ NeuralNetwork/predictions-train.py
@@ -1,16 +1,3 @@
 import numpy as np
 import pandas as pd
 
-weight_height = pd.read_csv('weight-height.csv')
-weight_height = weight_height.sample(frac=1)
-weight_height= weight_height.drop(labels=[*range(1000, 10000)], axis=0)
-weight_height.loc[(weight_height.Gender == "Female"), 'Gender'] = 0
-weight_height.loc[(weight_height.Gender == "Male"), 'Gender'] = 1
-
-wh = weight_height.sample(frac=1)
-def mse_loss(y_true, y_pred):
-    '''Функция потерь(среднеквадратическая ошибка)'''
-    return ((y_true - y_pred)**2).mean()
-
-y_true = np.array([ind for ind in wh['Gender']])
-y_pred = np.array([0 for i in range(len(wh['Gender']))])
